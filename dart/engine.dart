2f07f12baf499d3fdf7c710fcebb4aabc10dc0ed
Adrien Carta <adrien.carta@laposte.net>
2014-01-04 23:08:28 +0100

    add dart folder and component implementation in dart

---
+++ dart/engine.dart
@@ -0,0 +1,12 @@
+import "component/component.dart";
+import 'component/ComponentDefinition.dart';
+
+void main() {  
+  component.define("myFirstComponent", (myArg){
+    print(myArg);
+    return new Map();
+  });
+  ComponentDefinition lel = component.get("myFirstComponent");
+  lel.add(0, "yolo");
+  
+}
