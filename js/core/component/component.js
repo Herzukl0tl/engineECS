5721815818441ac15606f1a8994cbdcc523ab35c
Cyril Sobierajewicz <sobierajewicz.cyril@gmail.com>
2014-01-05 01:25:10 +0100

    throw error if trying to access an undefined ComponentDefinition or override an existing definition

--- js/core/component/component.js
+++ js/core/component/component.js
@@ -1,10 +1,18 @@
 function component(name) {
-  return component._definitions[name] || null;
+  if (name in component._definitions) {
+    return component._definitions[name];
+  }
+
+  throw new Error();
 }
 
 component._definitions = Object.create(null);
 
 component.define = function componentDefine(name, definition) {
+  if (name in factory._definitions) {
+    throw new Error();
+  }
+
   var componentDefinition = new ComponentDefinition(name, definition);
 
   component._definitions[name] = componentDefinition;
