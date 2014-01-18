fd01df609ce22db5a2eb3022f0b7196e816c4220
Cyril Sobierajewicz <sobierajewicz.cyril@gmail.com>
2014-01-18 04:06:24 +0100

    [Build] Hide the 'browserify' and 'uglify' tasks inside the 'buid:js' task. Remove the --js and --dart options of the 'lint', 'build' and 'watch' tasks. lib/dart-sdk/bin MUST be in your PATH for 'build:dart' to work.

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,21 +2,12 @@
 
 var gulp = require('gulp'),
   jshint = require('gulp-jshint'),
-  exec = require('gulp-exec'),
   browserify = require('gulp-browserify'),
   uglify = require('gulp-uglify'),
   clean = require('gulp-clean'),
   dart2js = require('gulp-dart2js'),
   rename = require('gulp-rename'),
   pkg = require('./package.json');
-
-['default', 'watch', 'build', 'lint'].forEach(function (name) {
-  gulp.task(name, function () {
-    if ('js' in gulp.env) gulp.run(name + ':js');
-    else if ('dart' in gulp.env) gulp.run(name + ':dart');
-    else gulp.run(name + ':js', name + ':dart');
-  });
-});
 
 gulp.task('lint:js', function () {
   return gulp.src(['./Gulpfile.js', './src/js/**/*.js'])
@@ -29,21 +20,15 @@ gulp.task('clean:js', function () {
     .pipe(clean());
 });
 
-gulp.task('browserify', ['clean:js'], function () {
+gulp.task('build:js', ['lint:js', 'clean:js'], function () {
   return gulp.src('./src/js/*.js')
     .pipe(browserify())
     .pipe(rename(pkg.name + '.js'))
-    .pipe(gulp.dest('./dist/js'));
-});
-
-gulp.task('uglify', ['browserify'], function () {
-  gulp.src('./dist/js/' + pkg.name + '.js')
+    .pipe(gulp.dest('./dist/js'))
     .pipe(uglify())
     .pipe(rename(pkg.name + '.min.js'))
     .pipe(gulp.dest('dist/js'));
 });
-
-gulp.task('build:js', ['lint:js', 'uglify']);
 
 gulp.task('watch:js', function () {
   gulp.watch(['./Gulpfile.js', './src/js/**/*.js'], function () {
@@ -55,22 +40,15 @@ gulp.task('default:js', ['build:js'], function () {
   gulp.run('watch:js');
 });
 
-gulp.task('lint:dart', function () {
-  return gulp.src('./')
-    .pipe(exec('dartanalyzer ./src/dart/**/*.dart'));
-});
-
 gulp.task('clean:dart', function () {
   return gulp.src('./dist/dart/*', {read: false})
     .pipe(clean());
 });
 
-gulp.task('dart2js', ['clean:dart'], function () {
-  gulp.src('./src/dart/*.dart')
+gulp.task('build:dart', ['clean:dart'], function () {
+  return gulp.src('./src/dart/*.dart')
     .pipe(dart2js('./dist/dart/'));
 });
-
-gulp.task('build:dart', ['lint:dart', 'dart2js']);
 
 gulp.task('watch:dart', function () {
   gulp.watch('./src/dart/**/*.dart', function () {
@@ -80,4 +58,14 @@ gulp.task('watch:dart', function () {
 
 gulp.task('default:dart', ['build:dart'], function () {
   gulp.run('watch:dart');
+});
+
+['build', 'watch'].forEach(function (name) {
+  gulp.task(name, [name + ':js', name + ':dart']);
+});
+
+gulp.task('default', function () {
+  if ('js' in gulp.env) gulp.run('default:js');
+  else if ('dart' in gulp.env) gulp.run('default:dart');
+  else gulp.run('default:js', 'default:dart');
 });
