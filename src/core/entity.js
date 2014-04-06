bff745681da37cb6849486943e2e149ce4b1b2cb
Herzucco <herzucco@gmail.com>
2014-04-06 21:28:40 +0200

    merge and clean entity part of nuclear core

--- src/core/entity.js
+++ src/core/entity.js
@@ -1,5 +1,45 @@
 'use strict';
 
-function Entity() {}
+var nuclearEntityGenerator = new (require('./nuclearEntity-id-generator'))(0);
+
+/**
+ * The Entity constructor
+ * @param {string} name   The Entity name
+ * @param {Object} source The Entity config
+ */
+function Entity(name, source) {
+  this.name = name;
+  this.definition = function defaultDefinition(entity, data){
+    
+  };
+}
+
+Entity.generator = nuclearEntityGenerator;
+
+/**
+ * Create an entity depending on this Entity
+ * @param  {object} options All the components data
+ * @return {number}         The created entity
+ */
+Entity.prototype.create = function EntityCreate(options) {
+  var id = Entity.generator.next();
+  this.definition(id, options);
+  
+  // entity.trigger('create:' + this.name, id);
+  // entity.trigger('create_entity', id, this.name);
+  return id;
+};
+
+/**
+ * Enhance an entity with this factory definition
+ * @param  {number} entity The entity to enhance
+ * @param  {object} data Data to configure components
+ * @return {number}            The entity to enhance
+ */
+Entity.prototype.enhance = function EntityDeSerialize(entity, data) {
+  this.definition(entity, data);
+
+  return entity;
+};
 
 module.exports = Entity;
--- src/core/nuclear.entity.js
+++ src/core/nuclear.entity.js
@@ -1,11 +1,102 @@
 'use strict';
 
-var registry;
+var registry = require('./nuclear.registry'),
+    nuclearComponent = require('./nuclear.component'),
+    Entity = require('./entity');
 
-registry = require('./nuclear.registry');
-
+/**
+ * The nuclearEntity method which contains all entities definitions
+ * This is also the nuclearEntity definition getter (throws an error if the Entity doesn't exist)
+ * @param  {string} name The Entity name
+ * @return {object}      The selected Entity
+ */
 function nuclearEntity(name) {
-  return registry.entity(name);
+  if (name in nuclearEntity._definitions) {
+    return registry.nuclearEntity(name);
+  }
+
+  throw new Error();
+}
+
+nuclearEntity._definitions = Object.create(null);
+
+/**
+ * Define an Entity
+ * @param  {string} name   The Entity name
+ * @param  {object} source The Entity config
+ * @return {Entity}        The defined Entity
+ */
+nuclearEntity.define = function nuclearEntityDefine(name, source) {
+  if (name in nuclearEntity._definitions) {
+    throw new Error();
+  }
+
+  var entityDefinition = new Entity(name, source);
+
+  nuclearEntity._definitions[name] = entityDefinition;
+
+  return entityDefinition;
+};
+
+/**
+ * Serialize the selected nuclearEntity
+ * @param  {number} id The selected nuclearEntity
+ * @return {string}    The serialized nuclearEntity
+ */
+nuclearEntity.serialize = function nuclearEntitySerialize(id) {
+  var serialized = Object.create(null),
+    components = component.all(entity); //change .of to .all here
+
+  serialized.id = entity;
+  serialized.options = Object.create(null);
+
+  for (var i = components.length - 1; i > 0; i--) {
+    var name = components[i];
+    var definition = component(name);
+    var data = definition.of(entity);
+
+    if (typeof data.toJSON === 'function') data = data.toJSON();
+    serialized.options[name] = data;
+  }
+
+  return JSON.stringify(serialized);
+};
+
+/**
+ * Deserialize a serialized nuclearEntity
+ * @param  {string} serialized The serialized nuclearEntity
+ * @return {number}            The created nuclearEntity id
+ */
+nuclearEntity.deSerialize = function nuclearEntityDeSerialize(serialized) {
+  var id = nuclearEntity.create(components.options);
+
+  return id;
+};
+
+/**
+ * Remove the selected nuclearEntity and its components
+ * @param  {number} id The selected nuclearEntity
+ * @return {boolean}    Return true
+ */
+nuclearEntity.remove = function nuclearEntityRemove(id) {
+  var components = component.of(id);
+  
+  for (var i = components.length - 1; i >= 0; i -= 1) {
+    component(components[i]).remove(id);
+  }
+
+  // nuclearEntity.trigger('remove:' + factory.name, id);
+  return true;
+};
+
+nuclearEntity.create = function nuclearEntityCreate(options){
+  var id = Entity.generator.next(),
+      i;
+  for(var i in options){
+    nuclearComponent(i).add(id, options[i]);
+  }
+  
+  return id;
 }
 
 module.exports = nuclearEntity;
